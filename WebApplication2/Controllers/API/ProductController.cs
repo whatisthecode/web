2b65993a6cada0f3292b2ff5c2108695ed407749
ledinhnam <ledinhnam1131@gmail.com>
2017-10-21 12:01:19 +0700

    merge branch

---
+++ WebApplication2/Controllers/API/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication2.DAO;
+using WebApplication2.Models;
+using WebApplication2.Models.Mapping;
+
+namespace WebApplication2.Controllers.API
+{
+    public class ProductController : ApiController
+    {
+        private ProductDAO productdao;
+        [Route("api/product")]
+        [HttpPost]
+        IHttpActionResult insertnewProduct([FromBody]Product product)
+        {
+            Response response = new Response();
+            return 1;
+        }
+    
+
+
+
+    }
+}
---
+++ WebApplication2/DAO/ProductDAO.cs
@@ -0,0 +1,23 @@
+﻿using LaptopWebsite.Models.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication2.Models;
+
+namespace WebApplication2.DAO
+{
+    public interface ProductDAO
+    {
+        IEnumerable<Product> getProducts();
+        Product getProduct(Int16 id);
+        void updateProduct(Product product);
+        void insertProduct(Product product);
+        void deleteProduct(Int16 id);
+        void save();
+        void dispose();
+        Product checkexist(Product product);
+        PagedResult<Product> PageView(Int16 indexnum, Int16 pagesize, String Orderby);
+        PagedResult<Product> PageView(Int16 indexnum, Int16 pagesize, String Orderby, Boolean ascending);
+    }
+}
