b0b3d4ade6acf8b5cbc17b3e5f4ce75ae3c8f274
ngodonghong <ngodonghong@gmail.com>
2017-11-27 14:39:47 +0200

    validate form set passowrd

--- WebApplication2/Scripts/Client/Controller/SetPassword.js
+++ WebApplication2/Scripts/Client/Controller/SetPassword.js
@@ -1,5 +1,37 @@
 ﻿if ("undefined" !== typeof app) {
-    app.controller("SetPasswordController", function ($scope) {
-        
+    app.controller("SetPasswordController", function ($scope, $http) {
+        $scope.data = {
+            "email" : "",
+            "newPassword": "",
+            "confirmPassword" : ""
+        }
+
+        $scope.confirmPassword = function (password, confirmPassword) {
+            console.log("a");
+            if (password === confirmPassword)
+            {
+                return true;
+            }
+            else
+            {
+                return false
+            }
+        }
+
+        $scope.setPassword = function () {
+            var req = {
+                method: 'POST',
+                url: 'http://localhost:54962/api/Account/SetPassword',
+                headers: {
+                    'Content-Type': "application/json"
+                },
+                data: $scope.data
+            }
+            $http(req).then(function (response) {
+                console.log(response);
+            }, function (response) {
+                console.log(response);
+            });
+        }
     });
 }
