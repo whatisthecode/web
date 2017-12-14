e78c876419b90034ca8cd42e1eddb2406df6c473
Uncle's Hung <hungtienphan1995@gmail.com>
2017-12-14 15:56:41 +0700

    Update sidebar

--- WebApplication2/Controllers/Admin/AdminLoginController.cs
+++ WebApplication2/Controllers/Admin/AdminLoginController.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Formatting;
 using System.Net.Http.Headers;
@@ -7,6 +8,7 @@ using System.Web.Mvc;
 using WebApplication2.CustomAttribute;
 using WebApplication2.Models;
 using WebApplication2.Models.Mapping;
+using WebApplication2.Models.RequestModel;
 using static WebApplication2.Models.AccountViewModels;
 
 namespace WebApplication2.Controllers.Admin
@@ -16,6 +18,7 @@ namespace WebApplication2.Controllers.Admin
         static Uri baseUrl = new Uri("http://localhost:54962/");
         public AdminLoginController()
         {
+
         }
         // GET: Default
         [MVCAuthorize]
@@ -59,8 +62,64 @@ namespace WebApplication2.Controllers.Admin
                 {
                     var content2 = ((JObject)response2.Content.ReadAsAsync<Response>().Result.results).ToObject<UserInfoViewModel>();
                     CurrentUserInfoLogin userInfo = ((JObject)content2.userInfo).ToObject<CurrentUserInfoLogin>();
+                    Boolean isCustomer = true;
+                    Boolean isSuperAdmin = false;
+                    Boolean isAdmin = false;
+                    Boolean isMerchant = false;
+                    foreach(var userGroup in userInfo.groups)
+                    {
+                        if(userGroup.Group.name == "Merchant")
+                        {
+                            isCustomer = false;
+                            isMerchant = true;
+                            break;
+                        }
+                        else if(userGroup.Group.name == "Admin")
+                        {
+                            isCustomer = false;
+                            isAdmin = true;
+                        }
+                        else if(userGroup.Group.name == "SuperAdmin")
+                        {
+                            isCustomer = false;
+                            isSuperAdmin = true;
+                        }
+                    }
+                    List<SidebarModel> sidebar = new List<SidebarModel>();
+                    if (isSuperAdmin)
+                    {
+                        sidebar.Add(new SidebarModel("Người dùng", "/dashboard/users?pageSize=10&pageIndex=1", "ti-user", "user"));
+                        sidebar.Add(new SidebarModel("Sản phẩm", "/dashboard/products?pageSize=10&pageIndex=1", "ti-package", "product"));
+                        sidebar.Add(new SidebarModel("Đơn hàng", "/dashboard/invoices?pageSize=10&pageIndex=1", "ti-shopping-cart-full", "invoice"));
+                        sidebar.Add(new SidebarModel("Thông tin cá nhân", "/dashboard/info", "ti-id-badge", "info"));
+                        sidebar.Add(new SidebarModel("Đăng xuất", "/logout", "ti-shift-right", "logout"));
+                    }
+                    else if (isAdmin)
+                    {
+                        sidebar.Add(new SidebarModel("Người dùng", "/dashboard/users?pageSize=10&pageIndex=1", "ti-user", "user"));
+                        sidebar.Add(new SidebarModel("Sản phẩm", "/dashboard/products?pageSize=10&pageIndex=1", "ti-package", "product"));
+                        sidebar.Add(new SidebarModel("Đơn hàng", "/dashboard/invoices?pageSize=10&pageIndex=1", "ti-shopping-cart-full", "invoice"));
+                        sidebar.Add(new SidebarModel("Thông tin cá nhân", "/dashboard/info", "ti-id-badge", "info"));
+                        sidebar.Add(new SidebarModel("Đăng xuất", "/logout", "ti-shift-right", "logout"));
+                    }
+                    else if (isCustomer)
+                    {
+                        sidebar.Add(new SidebarModel("Đơn hàng","/dashboard/invoices?pageSize=10&pageIndex=1", "ti-shopping-cart-full", "invoice"));
+                        sidebar.Add(new SidebarModel("Thông tin cá nhân","/dashboard/info", "ti-id-badge", "info"));
+                        sidebar.Add(new SidebarModel("Nâng cấp tài khoản","/upgrade", "ti-rocket", "upgrade"));
+                        sidebar.Add(new SidebarModel("Đăng xuất","/logout", "ti-shift-right", "logout"));
+                    }
+                    else if (isMerchant)
+                    {
+                        sidebar.Add(new SidebarModel("Sản phẩm", "/dashboard/products?pageSize=10&pageIndex=1", "ti-package", "product"));
+                        sidebar.Add(new SidebarModel("Đơn hàng", "/dashboard/invoices?pageSize=10&pageIndex=1", "ti-shopping-cart-full", "invoice"));
+                        sidebar.Add(new SidebarModel("Thông tin cá nhân", "/dashboard/info", "ti-id-badge", "info"));
+                        sidebar.Add(new SidebarModel("Mua gói tin", "/buy", "ti-money", "buy"));
+                        sidebar.Add(new SidebarModel("Đăng xuất", "/logout", "ti-shift-right", "logout"));
+                    }
+                    Session["sidebar"] = sidebar;
                     Session["username"] = userInfo.firstName + " " + userInfo.lastName;
-
+                    
                     if (!String.IsNullOrEmpty(returnUrl))
                     {
                         return Redirect(returnUrl);
@@ -88,6 +147,7 @@ namespace WebApplication2.Controllers.Admin
             {
                 Session["currentUser"] = null;
                 Session["username"] = null;
+                Session["sidebar"] = null;
             }
             return RedirectToAction("Index", "AdminLogin");
         }
---
+++ WebApplication2/Controllers/Admin/AdminStatisticController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers.Admin
+{
+    public class AdminStatisticController : Controller
+    {
+        // GET: AdminStatistic
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
