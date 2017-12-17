8217f78e05b929833c2b84c2e8b61eb473489ebf
ngodonghong <ngodonghong@gmail.com>
2017-12-17 14:32:51 -0800

    pull code to update

--- WebApplication2/Scripts/Client/Controller/Cart.js
+++ WebApplication2/Scripts/Client/Controller/Cart.js
@@ -229,16 +229,16 @@
             }
         };
     })
-        .directive('owlCarouselItem', [function () {
-            return {
-                restrict: 'A',
-                transclude: false,
-                link: function (scope, element) {
-                    // wait for the last item in the ng-repeat then call init
-                    if (scope.$last) {
-                        scope.initCarousel(element.parent());
-                    }
+    .directive('owlCarouselItem', [function () {
+        return {
+            restrict: 'A',
+            transclude: false,
+            link: function (scope, element) {
+                // wait for the last item in the ng-repeat then call init
+                if (scope.$last) {
+                    scope.initCarousel(element.parent());
                 }
-            };
-        }]);
+            }
+        };
+    }]);
 }
